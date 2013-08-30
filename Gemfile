69f30026b52b656363e8b6a113d7f7b655dc8654
T-zZ <tozz_75@hotmail.com>
2013-08-30 13:47:00 +0200

    ...

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ gem 'refinerycms-i18n', '~> 2.0.0'
 #  gem 'refinerycms-search', '~> 2.0.0'
    gem 'refinerycms-page-images', '~> 2.0.0'
 
+# Inuitcss
+  gem 'skeleton-rails' 
--- vendor/assets/javascripts/test.js
+++ vendor/assets/javascripts/test.js
@@ -1,29 +1,4 @@
 //$(document).ready(function() {
 $(function() {
 
- $('.container').animate(
-   {
-     right: '150px',
-     opacity: .5,
-     fontSize: '24px'
-   },
-   1500
-   );
- 
- $('.container__form').hide();
- $('.container__title').hover(function(){
-    $('.container__form').slideToggle();
- }); // end toggle menu
-
-
- $('.answer').hide();
- $('.main h2').toggle(
-  function() {
-    $('.answer').slideDown('slow');
-  },
-  function() {
-    $(this).next('.answer').slideUp();
-  }
-  );
-
 }); // end ready
